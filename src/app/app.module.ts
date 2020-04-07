5bc883f4ba8edbca4b3814e295bd5f510a448aef
fadzreen <fadzreen@kollect.my>
2020-04-08 00:27:22 +0800

    Create Angular UI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { HttpModule } from '@angular/http';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { TaskViewComponent } from './pages/task-view/task-view.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskViewComponent
   ],
   imports: [
     BrowserModule,
